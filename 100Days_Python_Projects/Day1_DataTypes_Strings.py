0089b42dc313387f8f28ff86aff7ef3d65e65a3e
Szafranerio <93886616+Szafranerio@users.noreply.github.com>
2024-08-20 18:40:41 +0200

    Add files via upload

---
+++ 100Days_Python_Projects/Day1_DataTypes_Strings.py
@@ -0,0 +1,27 @@
+print('Hello World!')
+
+print('Hello World!\nHello World!')
+
+print('Hello' + " " + 'World!')
+
+# Input function
+print('What is your name?')
+name = input('Write your name')  # Enter the prompt
+print('Hello', name)
+
+# The other way of print and input
+print('Hello ' + input('What is your name?'))
+
+# len()
+word = input('What is your word?')
+length = len(word)
+print(length)
+
+# Brand name generator
+print('Welcom to the brand name generator!')
+name = input('What is your name?')
+print('Hello ', name, 'nice to meet you!')
+print(' You came here to find the name for your music band!')
+city = input('Where are you from?')
+animal = input('What is the name of your first pet?')
+print('There is the name for your new band: ' + city + " " + animal)
