c3d0cf15e1310eee7ea19da942816b6097992203
Szafranerio <93886616+Szafranerio@users.noreply.github.com>
2024-08-23 13:17:41 +0200

    Add files via upload

---
+++ 100Days_Python_Projects/Day5_PythonLoops_PasswordGenerator.py
@@ -0,0 +1,83 @@
+import random
+student_score = [150, 12, 56, 87, 65, 32, 145,
+                 67, 123, 45, 87, 98, 187, 87, 65, 90, 34, 56]
+
+sum = 0
+for score in student_score:
+    sum += score
+print(sum)
+
+max_score = 0
+for score in student_score:
+    if score > max_score:
+        max_score = score
+print(max_score)
+
+# Range
+
+for number in range(1, 10, 2):
+    print(number)
+
+# Gauss challange
+
+sum = 0
+for number in range(1, 101):
+    sum += number
+print(sum)
+
+# FizzBuzz
+
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print('FizzBuzz')
+    elif number % 3 == 0:
+        print('Fizz')
+    elif number % 5 == 0:
+        print('Buzz')
+    else:
+        print(number)
+
+
+# Password generator
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = int(input('How many letters you would like to have?: '))
+nr_number = int(input('How many numbers you would like to have?: '))
+nr_symbols = int(input('How many symbols you would like to have?: '))
+
+password = ''
+
+for char in range(0, nr_letters):
+    password += random.choice(letters)
+
+for char in range(0, nr_number):
+    password += random.choice(numbers)
+
+for char in range(0, nr_symbols):
+    password += random.choice(symbols)
+
+print(password)
+
+# Hard
+password_list = []
+for char in range(0, nr_letters):
+    password_list.append(random.choice(letters))
+
+for char in range(0, nr_number):
+    password_list.append(random.choice(numbers))
+
+for char in range(0, nr_symbols):
+    password_list.append(random.choice(symbols))
+
+random.shuffle(password_list)
+print(password_list)
+
+password = ''
+for char in password_list:
+    password += char
+
+print(f'Your password is: {password}')
---
+++ 100Days_Python_Projects/Day8_Functions_CaesarCipher.py
@@ -0,0 +1,80 @@
+name = input('What is your name?: ')
+
+def greet(name):
+    print(f'Hello World {name}')
+    print('Hello Aalborg')
+    print('You are the best!')
+    
+greet(name)
+
+age = int(input('What is your age: '))
+
+def life_in_weeks(age):
+    
+    age_to_ninty = 90 - age 
+    weeks = age_to_ninty * 52
+    print(f'You have {weeks} weeks left')
+          
+life_in_weeks(age)
+
+
+
+def life_in_weeks(age):
+    
+    age_to_ninty = 90 - age 
+    weeks = age_to_ninty * 52
+    print(f'You have {weeks} weeks left')
+          
+life_in_weeks(27)
+
+
+def greet_with(name, location):
+    print(f'Hello {name}')
+    print(f'Are you in {location}?')
+    
+greet_with(location='Aalborg', name = 'Bartek')
+
+#Cesar cipher 
+
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+    'y', 'z'
+]
+
+direction = input('Type encode to code the message or decode to see the message: ').lower()
+text = input('Type your message: ')
+shift = int(input('Type the shift number: '))
+    
+def ceaser(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    if encode_or_decode == "decode":
+                shift_amount *= -1
+    for letter in original_text:
+        
+        if letter not in alphabet:
+            output_text += letter
+        else:    
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+    print(f'Here is coded message {output_text}')
+        
+should_continiue = True
+
+while should_continiue:
+    direction = input('Type encode to code the message or decode to see the message: ').lower()
+    text = input('Type your message: ')
+    shift = int(input('Type the shift number: '))
+    
+        
+    ceaser(text, shift, direction)
+    restart = input('Do you want to go again?: y for Yes, n for No?\n').lower()
+    if restart == "n":
+        should_continiue = False
+        print('Goodbye')
+
+
+
+        
+
