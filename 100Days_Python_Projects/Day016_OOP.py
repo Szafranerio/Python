ed623f1d2466996a5bc809f11dae84e7b693b891
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-08-26 18:10:48 +0200

    updated version

---
+++ 100Days_Python_Projects/Day016_OOP.py
@@ -0,0 +1,18 @@
+#from turtle import Turtle, Screen
+#
+#timmy = Turtle()
+#timmy.shape('turtle')
+#timmy.color('DarkGoldenrod1')
+#timmy.forward(100)
+#
+#my_screen = Screen()
+#
+#my_screen.exitonclick()
+ 
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+
+
+print(table)
