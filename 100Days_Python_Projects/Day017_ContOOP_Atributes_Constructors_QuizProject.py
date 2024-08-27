e217e6624a73dbfc03ad67f333a760bbe77dcb79
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-08-27 19:01:32 +0200

    update oop

--- 100Days_Python_Projects/Day017_ContOOP_Atributes_Constructors_QuizProject.py
+++ 100Days_Python_Projects/Day017_ContOOP_Atributes_Constructors_QuizProject.py
@@ -1,10 +1,19 @@
-class User:
-    pass
+#Constructor __init__(self)
+class User():
+    def __init__(self, user_id, username, user_age, user_sex):
+        self.id = user_id
+        self.name = username
+        self.age = user_age
+        self.sex = user_sex
+        self.followers = 0
+        self.following = 0
+    
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+    
+user_1 = User('001', 'Szafranerio', 24, 'Male')
+
+print(f'You are {user_1.name}, your id is {user_1.id} You are allowed to drink, because you are {user_1.age}. You are {user_1.sex}, and you are following {user_1.following} users')
 
 
-user_1 = User()
-user_1.id = "001"
-user_1.username = 'Szafranerio'
-user_1.color = 'yellow'
-
-print(user_1.username)
