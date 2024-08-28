a94db78ed7c5303262cac45da60c8264087ee62b
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-08-28 22:03:09 +0200

    snake update

--- Object-Oriented-Programming/Day020_Snake/snake.py
+++ Object-Oriented-Programming/Day020_Snake/snake.py
@@ -25,4 +25,21 @@ class Snake:
         for seg_num in range(len(self.segments)-1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
-            self.segment
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+        
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+    
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+    
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+        
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
