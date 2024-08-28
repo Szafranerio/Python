bcc701b8e5d8f1679c4340dd4e8385e7d202eb94
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-08-28 15:02:50 +0200

    sketch project

--- 100Days_Python_Projects/Day019_Instances_HigherOrderFunction.py
+++ 100Days_Python_Projects/Day019_Instances_HigherOrderFunction.py
@@ -2,3 +2,27 @@ from turtle import Turtle, Screen
 
 tim = Turtle()
 screen = Screen()
+
+#Sketch project
+#def move_forward():
+#    tim.forward(10)
+#    
+#def move_back():
+#    tim.back(10)
+#    
+#def rotate():
+#    tim.right(45)
+#    
+#def rotate_back():
+#    tim.left(45)
+#    
+#def circle():
+#    tim.circle(50)
+#    
+#screen.listen()
+#screen.onkey(move_forward, "w")
+#screen.onkey(move_back, "s")
+#screen.onkey(rotate, "d")
+#screen.onkey(rotate_back, "a")
+#screen.onkey(circle, "o")
+#screen.exitonclick()
