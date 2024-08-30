96a008f84e729e3e205bfd943837209258049a48
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-08-30 22:06:22 +0200

    updates

--- Object-Oriented-Programming/Day023_TurtleCrossingRoad/main.py
+++ Object-Oriented-Programming/Day023_TurtleCrossingRoad/main.py
@@ -7,8 +7,33 @@ from scoreboard import Scoreboard
 screen = Screen()
 player = Player()
 car_manager = CarManager()
+scoreborad = Scoreboard()
 
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
-screen.onkey(player.go_down, "Down
+screen.onkey(player.go_down, "Down")
+
+screen.setup(width=600, height=600)
+screen.tracer(0)
+screen.bgcolor('white')
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    
+    car_manager.create_car()
+    car_manager.move_cars()
+    
+    
+    #Collision
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+    
+    if player.finish_line():
+        player.restart()
+        car_manager.level_up()
+    
+screen.exitonclick()
--- Object-Oriented-Programming/Day023_TurtleCrossingRoad/scoreboard.py
+++ Object-Oriented-Programming/Day023_TurtleCrossingRoad/scoreboard.py
@@ -4,8 +4,22 @@ FONT = ("Courier", 24, "normal")
 class Scoreboard(Turtle):
     
     def __init__(self):
-        self.level = []
+        super().__init__()
+        self.color('white')
+        self.penup()
+        self.hideturtle()
+        level_count = 1
+        self.update_score()
         
     def score(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.level_count, align='center',
+                   font=('Courier', 80, 'normal'))
+    
+    def next_level(self):
+        self.level_score += 1
+        self.score
+    
         
     pass
