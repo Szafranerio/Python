d352f06fee0b92c7a3fcca66519e2f24464b219a
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-08-31 11:28:53 +0200

    high score update and game reset

--- Object-Oriented-Programming/Day020_Snake/scoreboard.py
+++ Object-Oriented-Programming/Day020_Snake/scoreboard.py
@@ -10,6 +10,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.penup()
         self.color('white')
         self.goto(POSITION)
@@ -17,18 +18,13 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
         
     def update_scoreboard(self):
-        self.write(f"Score: {self.score} ", align=ALLIGMENT, font=FONT)
-        
-    def game_over(self):
-        self.goto(0, 0)
-        self.write('Game over', align=ALLIGMENT, font=FONT)
-         
-    def increase_score(self):
-        self.score += 1
         self.clear()
+        self.write(f"Score: {self.score}   High score is {self.high_score}", align=ALLIGMENT, font=FONT)
+        
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
         self.update_scoreboard()
-        
-   
-        
-        
-        
+         
+    def increase
