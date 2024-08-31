55dee8f6683853a0639058807383207a2b2d4542
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-08-31 12:23:23 +0200

    hiugh score

--- Object-Oriented-Programming/Day020_Snake/scoreboard.py
+++ Object-Oriented-Programming/Day020_Snake/scoreboard.py
@@ -10,7 +10,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.color('white')
         self.goto(POSITION)
@@ -24,15 +25,4 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
-        self.score = 0
-        self.update_scoreboard()
-         
-    def increase_score(self):
-        self.score += 1
-        self.clear()
-        self.update_scoreboard()
-        
-   
-        
-        
-        
+            with open('d
