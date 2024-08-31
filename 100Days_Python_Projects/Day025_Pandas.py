bc5d8fed2958e23887ef405c3bdd91e6774db6b3
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-08-31 18:45:15 +0200

    end_game

--- 100Days_Python_Projects/Day025_Pandas.py
+++ 100Days_Python_Projects/Day025_Pandas.py
@@ -17,8 +17,18 @@ all_states = data.state.to_list()
 guessed_states = []
 
 while len(guessed_states) < 50:
-    answer_state = screen.textinput(title=f'{len(guessed_states)}/50 Guess the state', prompt='Enter the name of the state').capitalize()
+    answer_state = screen.textinput(title=f'{len(guessed_states)}/50 Guess the state', prompt='Enter the name of the state').title()
 
+    if answer_state == 'Exit':
+        missing_states= []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        print(missing_states)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t= turtle.Turtle()
@@ -27,5 +37,4 @@ while len(guessed_states) < 50:
         state_data = data[data.state == answer_state]
         t.goto(state_data.x.item(), state_data.y.item())
         t.write(answer_state)
-   
-screen.mainloop()
+        
