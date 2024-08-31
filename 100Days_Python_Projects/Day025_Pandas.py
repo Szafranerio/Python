ecf8eeb339b2a0eeeb1d734bd1cae57efbe44a4c
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-08-31 18:36:45 +0200

    us guess game update

--- 100Days_Python_Projects/Day025_Pandas.py
+++ 100Days_Python_Projects/Day025_Pandas.py
@@ -1,12 +1,31 @@
 #US state game
 
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title('U.S State Game')
-image = "./data/Day025_US_StateGame/blank_states_img.gif"
+
+#Path
+image = "/Users/bartlomiejszafran/Desktop/GitHub/Python/100Days_Python_Projects/data/Day025_US_StateGame/blank_states_img.gif"
 screen.addshape(image)
 
 turtle.shape(image)
 
-screen.exitonclick()
+data = pandas.read_csv('/Users/bartlomiejszafran/Desktop/GitHub/Python/100Days_Python_Projects/data/Day025_US_StateGame/50_states.csv')
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f'{len(guessed_states)}/50 Guess the state', prompt='Enter the name of the state').capitalize()
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t= turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
+   
+screen.mainloop()
