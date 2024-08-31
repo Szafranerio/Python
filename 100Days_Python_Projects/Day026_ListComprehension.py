64c1d14ff332d08d199835aab3d11ad791f4de4b
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-08-31 21:42:00 +0200

    more dict comprehension

--- 100Days_Python_Projects/Day026_ListComprehension.py
+++ 100Days_Python_Projects/Day026_ListComprehension.py
@@ -42,7 +42,22 @@ print(result)
 #new_dict = {new_key:new_value for (key,value) in dict.items()}
 #new_dict = {new_key:new_value for (key,value) in dict.items() if test}
 
+import random
 names = ['Alex', 'Beth', 'Mark', 'Thomas', 'Patrick']
 
-grades = {}
+scores = {student:random.randint(1,100) for student in names}
+print(scores)
+
+passed = {student:score for (student, score) in scores.items() if score >=50 }
+print(passed)
+
+#Dictionary with the length of words
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+result = {word:len(word) for word in sentence.split()}
+print(result)
+
+weather_c = {'Monday':12, 'Tuesday':14, 'Wednsday':15, 'Thursday':14, 'Friday': 21, 'Saturday':22, 'Sunday':24}
+weather_f = {day:(temp_c * 9/5 + 32 ) for (day, temp_c) in weather_c.items()}
+print(weather_f)
+
 
