f1963ee98185cfa763026b9c1d53313d308de609
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-08-31 21:15:35 +0200

    list comprehension added

--- 100Days_Python_Projects/Day025_Pandas.py
+++ 100Days_Python_Projects/Day025_Pandas.py
@@ -20,10 +20,8 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f'{len(guessed_states)}/50 Guess the state', prompt='Enter the name of the state').title()
 
     if answer_state == 'Exit':
-        missing_states= []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
+    
         print(missing_states)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
