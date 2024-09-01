a42f15467b755a32ac3aa542d5cf8f7180c3a715
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-01 13:48:55 +0200

    end project final code

--- 100Days_Python_Projects/Day027_Tkinter_*args_*kwargs_GUI.py
+++ 100Days_Python_Projects/Day027_Tkinter_*args_*kwargs_GUI.py
@@ -1,34 +1,56 @@
 import tkinter
 
+#window = tkinter.Tk()
+#window.title('GUI Program')
+#window.minsize(width=500, height=300)
+#
+##Label
+#my_label = tkinter.Label(text='Label', font=('Arial', 24, 'bold'))
+#my_label.grid(column=0,row=0)
+#
+##Entry
+#input = tkinter.Entry(width=10)
+#input.grid(column=2, row=2)
+#
+##Buttons
+#def button_clicked():
+#    print('I got clicked')
+#    new_text = input.get()
+#    my_label.config(text=new_text)
+#    
+#button = tkinter.Button(text='Click here', command=button_clicked)
+#button.grid(column=1, row=1)
+#
+#button_1 = tkinter.Button(text='New Button')
+#button_1.grid(column=2, row=0)
+
+#Converter project
+
 window = tkinter.Tk()
 window.title('GUI Program')
 window.minsize(width=500, height=300)
 
-#Label
-my_label = tkinter.Label(text='Label', font=('Arial', 24, 'bold'))
-my_label.grid(column=0,row=0)
-
-#Entry
 input = tkinter.Entry(width=10)
-input.grid(column=2, row=2)
+input.grid(column=1,row=0)
 
-#Buttons
-def button_clicked():
-    print('I got clicked')
-    new_text = input.get()
-    my_label.config(text=new_text)
-    
-button = tkinter.Button(text='Click here', command=button_clicked)
-button.grid(column=1, row=1)
+miles = tkinter.Label(text='miles')
+miles.grid(column=2,row=0)
 
-button_1 = tkinter.Button(text='New Button')
-button_1.grid(column=2, row=0)
+is_equal = tkinter.Label(text='is equal to: ')
+is_equal.grid(column=0, row=2)
 
+value = tkinter.Label(text = 0)
+value.grid(column=1, row=2)
 
+km = tkinter.Label(text='km')
+km.grid(column=2, row=2)
 
+def convertor():
+    new_value = float(input.get()) * 1.609
+    value.config(text=new_value)
 
-
-
+button = tkinter.Button(text='Calculate', command=convertor)
+button.grid(column=1, row=3)
 
 
 
