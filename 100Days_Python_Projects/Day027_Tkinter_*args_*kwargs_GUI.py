be9c0d5c7724614888be73413626b8578c6bbe0f
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-01 12:22:06 +0200

    intro

---
+++ 100Days_Python_Projects/Day027_Tkinter_*args_*kwargs_GUI.py
@@ -0,0 +1,20 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title('GUI Program')
+window.minsize(width=500, height=300)
+
+#Label
+my_label = tkinter.Label()
+
+
+
+
+
+
+
+
+
+#At the end always
+window.mainloop()
+    
