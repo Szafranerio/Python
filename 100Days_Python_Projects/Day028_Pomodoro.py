ea4ad8c2a875f3d678571e7a32a5d3dc6b9a67bd
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-01 16:10:00 +0200

    timer

--- 100Days_Python_Projects/Day028_Pomodoro.py
+++ 100Days_Python_Projects/Day028_Pomodoro.py
@@ -14,6 +14,11 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -23,12 +28,13 @@ window.config(padx=100, pady=50, bg=YELLOW)
 timer_text = Label(text='Timer', font=(FONT_NAME, 50), fg=GREEN, bg=YELLOW, highlightthickness=0)
 timer_text.grid(column=1, row=0)
 
-
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_png = PhotoImage(file='/Users/bartlomiejszafran/Desktop/GitHub/Python/100Days_Python_Projects/data/Day028_Pomodoro/tomato.png')
 canvas.create_image(100, 112, image=tomato_png)
-canvas.create_text(100,130, text="00:00", fill='white', font=(FONT_NAME, 35, 'bold'))
+timer_text = canvas.create_text(100,130, text="00:00", fill='white', font=(FONT_NAME, 35, 'bold'))
 canvas.grid(column=1, row=1)
+
+count_down(5)
 
 
 start_button = Button(text='Start', highlightthickness=0, borderwidth=0, highlightbackground=YELLOW)
