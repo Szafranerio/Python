0693efc7420f09ad1e700f2642a8e2d434ce94a3
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-01 13:18:37 +0200

    Entry, Buttons, Strings

--- 100Days_Python_Projects/Day027_Tkinter_*args_*kwargs_GUI.py
+++ 100Days_Python_Projects/Day027_Tkinter_*args_*kwargs_GUI.py
@@ -5,7 +5,27 @@ window.title('GUI Program')
 window.minsize(width=500, height=300)
 
 #Label
-my_label = tkinter.Label()
+my_label = tkinter.Label(text='Label', font=('Arial', 24, 'bold'))
+my_label.pack()
+
+#Entry
+input = tkinter.Entry(width=10)
+input.pack()
+
+#Buttons
+def button_clicked():
+    print('I got clicked')
+    new_text = input.get()
+    my_label.config(text=new_text)
+    
+button = tkinter.Button(text='Click here', command=button_clicked)
+button.pack()
+
+
+
+
+
+
 
 
 
