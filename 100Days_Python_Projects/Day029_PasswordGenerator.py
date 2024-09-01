4fe8de59d6e83be1bb9868616a7300533048580d
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-01 19:30:48 +0200

    end_password_generator

--- 100Days_Python_Projects/Day029_PasswordGenerator.py
+++ 100Days_Python_Projects/Day029_PasswordGenerator.py
@@ -6,6 +6,8 @@ FONT_NAME = "Courier"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 from random import choice, randint, shuffle 
+import pyperclip 
+
 def gen_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -23,6 +25,7 @@ def gen_password():
     password = "".join(password_list)
     
     input_pass.insert(0, password)
+    pyperclip.copy(password)
     
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
