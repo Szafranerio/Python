f604a9cee24ebbef9df02b4380bfab6933bf310c
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-02 16:07:05 +0200

    updates

--- 100Days_Python_Projects/Day005_PythonLoops_PasswordGenerator.py
+++ 100Days_Python_Projects/Day005_PythonLoops_PasswordGenerator.py
@@ -14,19 +14,16 @@ for score in student_score:
 print(max_score)
 
 # Range
-
 for number in range(1, 10, 2):
     print(number)
 
 # Gauss challange
-
 sum = 0
 for number in range(1, 101):
     sum += number
 print(sum)
 
 # FizzBuzz
-
 for number in range(1, 101):
     if number % 3 == 0 and number % 5 == 0:
         print('FizzBuzz')
@@ -37,9 +34,7 @@ for number in range(1, 101):
     else:
         print(number)
 
-
 # Password generator
-
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
            'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
