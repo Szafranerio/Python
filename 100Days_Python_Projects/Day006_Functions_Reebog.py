893db42d25d60203c4c829ca932943639ff94038
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-02 21:33:26 +0200

    updates in Day006_Functions_Reebog

--- 100Days_Python_Projects/Day006_Functions_Reebog.py
+++ 100Days_Python_Projects/Day006_Functions_Reebog.py
@@ -10,17 +10,39 @@ my_function()
 # Reebog project
 
 
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
+#def turn_right():
+#   turn_left()
+#   turn_left()
+#   turn_left()
+#
+#
+#while not at_goal():
+#   if right_is_clear():
+#       turn_right()
+#       move()
+#   elif front_is_clear():
+#       move()
+#   else:
+#       turn_left()
+        
+        
+        
+#Functions
+def greet_user():
+    print('Hello user')
+greet_user()
 
+def hello_user(name): #Using a var i brackets allows to make for example personalized greeting functions. It will accept any value that has name=...
+    print(f'Hello {name}')
+    
+hello_user('Tom')
 
-while not at_goal():
-    if right_is_clear():
-        turn_right()
-        move()
-    elif front_is_clear():
-        move()
-    else:
-        turn_left()
+name = input('What is your name: ')
+book = input('What is your favourit book: ')
+
+def user_fav_book(name, book):
+    
+    print(f'This is {name} and his favourit book is {book}')
+    
+user_fav_book(name, book)
+    
