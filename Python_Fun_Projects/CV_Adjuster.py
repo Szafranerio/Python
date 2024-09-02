866202461b62df7b9f8375a0a88ce35ea17494ab
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-02 14:04:55 +0200

    update

--- Python_Fun_Projects/CV_Adjuster.py
+++ Python_Fun_Projects/CV_Adjuster.py
@@ -16,4 +16,6 @@ company = input("What is the name of the company: ")
 with open('/Users/bartlomiejszafran/Desktop/CV/CV.docx') as cv_file:
     document = cv_file.read()
     print(document)
-    new_cv = document(PLACEHOLDER, po
+    new_cv = document(PLACEHOLDER, position).replace(COMPANY, company)
+    with open(f"/Users/bartlomiejszafran/Desktop/CV/Ready/Bartlomiej Szafran {company} CV.docx", mode="w") as completed_cv:
+            completed_cv.write(new_cv)
