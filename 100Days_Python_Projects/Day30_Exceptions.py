ff909dd50de4632c42f3e4f8b1a1c2025864952b
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-02 12:57:07 +0200

    expectations

--- 100Days_Python_Projects/Day30_Exceptions.py
+++ 100Days_Python_Projects/Day30_Exceptions.py
@@ -13,4 +13,18 @@
 #except FileNotFoundError:
 #    file = open('data.txt', "w")
 #    file.write('something')
-#except KeyError as er
+#except KeyError as error_message:
+#    print(f'The key {error_message} does not exisit')
+#    
+#    
+#
+
+height = float(input('What is your height: '))
+weight = int(input('What is your wieght: '))
+
+if height > 3:
+    raise ValueError ("Are you really over 3 meters?")
+
+bmi = weight / height ** 2
+
+
