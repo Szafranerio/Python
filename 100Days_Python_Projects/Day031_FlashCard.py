78ca4b80af654c4793445de697421d4497377d58
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-03 19:59:57 +0200

    name change

--- 100Days_Python_Projects/Day031_FlashCard.py
+++ 100Days_Python_Projects/Day031_FlashCard.py
@@ -31,4 +31,36 @@ def flip_card():
     canvas.itemconfig(card_background, image=back_card)
     
 def is_known():
-    to
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv('./data/Day031_FlashCards/data/words_to_learn.csv', index=False)
+    next_card()
+
+# User Interface
+window = Tk()
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+window.title('Flash Cards')
+
+flip_timer = window.after(3000, func=flip_card)
+
+canvas = Canvas(width=800, height=526, highlightthickness=0)
+front_card = PhotoImage(file='./data/Day031_FlashCards/images/card_front.png')
+back_card = PhotoImage(file='./data/Day031_FlashCards/images/card_back.png')
+card_background = canvas.create_image(400, 263, image=front_card)
+card_title = canvas.create_text(
+    400, 150, text='Title', font=('Ariel', 40, 'italic'))
+card_word = canvas.create_text(
+    400, 263, text='WORD', font=('Ariel', 60, 'bold'))
+canvas.config(bg=BACKGROUND_COLOR)
+canvas.grid(column=0, row=0, columnspan=2)
+
+# Button images
+ok_button = PhotoImage(file='./data/Day031_FlashCards/images/right.png')
+false_button = PhotoImage(file='./data/Day031_FlashCards/images/wrong.png')
+correct_button = Button(image=ok_button, highlightthickness=0, command=is_known)
+correct_button.grid(column=1, row=2)
+wrong_button = Button(image=false_button, highlightthickness=0, command=next_card)
+wrong_button.grid(column=0, row=2)
+
+next_card()
+window.mainloop()
