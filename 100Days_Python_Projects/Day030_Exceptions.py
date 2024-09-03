0f5862a46e4d40231dd77693a13040840f9a222d
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-03 19:59:20 +0200

    title update

---
+++ 100Days_Python_Projects/Day030_Exceptions.py
@@ -0,0 +1,28 @@
+# try: Something that might cause an expection
+
+# expect: Do this if there was an exception. Often to broad, make it more accuarte for example: ('FileNotFoundError")
+
+# else: Do this if there were no exceptions
+
+# finally: Do this no matter what happens
+
+# try:
+#    file = open('data.txt')
+#    a = {'key':'value'}
+#    print(a['dsad'])
+# except FileNotFoundError:
+#    file = open('data.txt', "w")
+#    file.write('something')
+# except KeyError as error_message:
+#    print(f'The key {error_message} does not exisit')
+#
+#
+#
+
+height = float(input('What is your height: '))
+weight = int(input('What is your wieght: '))
+
+if height > 3:
+    raise ValueError("Are you really over 3 meters?")
+
+bmi = weight / height ** 2
