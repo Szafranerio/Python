ac1e366e19624bb9bc07519bc6a3f0ac620e66e1
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-04 16:00:20 +0200

    update auto-mailing

--- 100Days_Python_Projects/Day032_MailingWithPython.py
+++ 100Days_Python_Projects/Day032_MailingWithPython.py
@@ -1,64 +1,89 @@
-#import smtplib
-#mail = 'bartek.test.97@gmail.com'
-#password = 'vsmt nnix jzwd vxgx'
+# import smtplib
+# mail = 'bartek.test.97@gmail.com'
+# password = 'vsmt nnix jzwd vxgx'
 #
 #
-#with  smtplib.SMTP("smtp.gmail.com") as connection: #Remeber to change the security options!!!
+# with  smtplib.SMTP("smtp.gmail.com") as connection: #Remeber to change the security options!!!
 #    connection.starttls()
 #    connection.login(user=mail, password=password)
 #    connection.sendmail(from_addr=mail, to_addrs='bartek.yest@yahoo.com', msg='Subject:Hello\n\nThis is body of the mail')
-    
-#import datetime as dt
+
+# import datetime as dt
 #
-#now = dt.datetime.now()
-#print(now)
+# now = dt.datetime.now()
+# print(now)
 #
-#date_of_birth = dt.datetime(year=1997, month=9, day=15)
-#print(date_of_birth)
+# date_of_birth = dt.datetime(year=1997, month=9, day=15)
+# print(date_of_birth)
 
 import smtplib
 import datetime as dt
 import random
 import pandas
 
-#mail = 'bartek.test.97@gmail.com'
-#password = 'vsmt nnix jzwd vxgx'
+# mail = 'bartek.test.97@gmail.com'
+# password = 'vsmt nnix jzwd vxgx'
 #
-#now = dt.datetime.now()
-#weekday = now.weekday()
-#if weekday == 2:
+# now = dt.datetime.now()
+# weekday = now.weekday()
+# if weekday == 2:
 #    with open ('/Users/bartlomiejszafran/Desktop/GitHub/Python/100Days_Python_Projects/data/Day032_Mailing/quotes.txt') as quotes_file:
 #        quotes = quotes_file.readlines()
 #        selected = random.choice(quotes)
-#        
-#    print(selected)       
+#
+#    print(selected)
 #    with  smtplib.SMTP("smtp.gmail.com") as connection: #Remeber to change the security options!!!
 #        connection.starttls()
 #        connection.login(user=mail, password=password)
 #        connection.sendmail(from_addr=mail, to_addrs='bartek.yest@yahoo.com', msg='Subject:Motivation\n\nHere is your motivation: {}'.format(selected))
-#  
+#
 
 
+import pandas as pd
+import datetime as dt
+import smtplib
+import random
+import os
+
+# Email credentials
 mail = 'bartek.test.97@gmail.com'
 password = 'vsmt nnix jzwd vxgx'
 
+# Get today's date
 today = dt.datetime.now()
 today_tuple = (today.month, today.day)
 
-data = pandas.read_csv('/Users/bartlomiejszafran/Desktop/GitHub/Python/100Days_Python_Projects/data/Day032_Mailing/birthdays.csv')
-birthday_dict = {(row['month'], row['day']): row for index, row in data.iterrows()}
+# Define relative path to the files
+csv_file_path = '/home/Szafranerio/Day032_Mailing/birthdays.csv'  # Adjusted path
 
-if today_tuple in birthday_dict:
-    birthday_person = birthday_dict[today_tuple]
-    file_path = f'/Users/bartlomiejszafran/Desktop/GitHub/Python/100Days_Python_Projects/data/Day032_Mailing/letter_templates/letter_{random.randint(1,3)}.txt'
+# Check if the file exists
+if not os.path.isfile(csv_file_path):
+    print(f"File not found: {csv_file_path}")
+else:
 
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person['name'])
-    
-    # Send the email
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=mail, password=password)
-        message = f'Subject: Happy Birthday\n\n{contents}'
-        connection.sendmail(from_addr=mail, to_addrs=birthday_person['email'], msg=message)
+    data = pd.read_csv(csv_file_path)
+    birthday_dict = {(row['month'], row['day'])
+                      : row for index, row in data.iterrows()}
+
+    if today_tuple in birthday_dict:
+        birthday_person = birthday_dict[today_tuple]
+        file_path = f'/home/Szafranerio/letter_templates/letter_{random.randint(1,3)}.txt'
+
+        if not os.path.isfile(file_path):
+            print(f"Letter template not found: {file_path}")
+        else:
+            with open(file_path) as letter_file:
+                contents = letter_file.read()
+                contents = contents.replace("[NAME]", birthday_person['name'])
+
+            with smtplib.SMTP("smtp.gmail.com") as connection:
+                connection.starttls()
+                connection.login(user=mail, password=password)
+                message = f'Subject: Happy Birthday\n\n{contents}'
+                connection.sendmail(
+                    from_addr=mail, to_addrs=birthday_person['email'], msg=message)
+
+            print(
+                f"Email sent to {birthday_person['name']} at {birthday_person['email']}.")
+    else:
+        print("No birthdays today.")
