ad1310cabd03da8fbeafb55829a901fe5133511b
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-04 15:28:35 +0200

    birthday_wisher

--- 100Days_Python_Projects/Day032_MailingWithPython.py
+++ 100Days_Python_Projects/Day032_MailingWithPython.py
@@ -19,21 +19,46 @@
 import smtplib
 import datetime as dt
 import random
+import pandas
+
+#mail = 'bartek.test.97@gmail.com'
+#password = 'vsmt nnix jzwd vxgx'
+#
+#now = dt.datetime.now()
+#weekday = now.weekday()
+#if weekday == 2:
+#    with open ('/Users/bartlomiejszafran/Desktop/GitHub/Python/100Days_Python_Projects/data/Day032_Mailing/quotes.txt') as quotes_file:
+#        quotes = quotes_file.readlines()
+#        selected = random.choice(quotes)
+#        
+#    print(selected)       
+#    with  smtplib.SMTP("smtp.gmail.com") as connection: #Remeber to change the security options!!!
+#        connection.starttls()
+#        connection.login(user=mail, password=password)
+#        connection.sendmail(from_addr=mail, to_addrs='bartek.yest@yahoo.com', msg='Subject:Motivation\n\nHere is your motivation: {}'.format(selected))
+#  
+
 
 mail = 'bartek.test.97@gmail.com'
 password = 'vsmt nnix jzwd vxgx'
 
-now = dt.datetime.now()
-weekday = now.weekday()
-if weekday == 2:
-    with open ('/Users/bartlomiejszafran/Desktop/GitHub/Python/100Days_Python_Projects/data/Day032_Mailing/quotes.txt') as quotes_file:
-        quotes = quotes_file.readlines()
-        selected = random.choice(quotes)
-        
-    print(selected)       
-    with  smtplib.SMTP("smtp.gmail.com") as connection: #Remeber to change the security options!!!
+today = dt.datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv('/Users/bartlomiejszafran/Desktop/GitHub/Python/100Days_Python_Projects/data/Day032_Mailing/birthdays.csv')
+birthday_dict = {(row['month'], row['day']): row for index, row in data.iterrows()}
+
+if today_tuple in birthday_dict:
+    birthday_person = birthday_dict[today_tuple]
+    file_path = f'/Users/bartlomiejszafran/Desktop/GitHub/Python/100Days_Python_Projects/data/Day032_Mailing/letter_templates/letter_{random.randint(1,3)}.txt'
+
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person['name'])
+    
+    # Send the email
+    with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(user=mail, password=password)
-        connection.sendmail(from_addr=mail, to_addrs='bartek.yest@yahoo.com', msg='Subject:Motivation\n\nHere is your motivation: {}'.format(selected))
-
-
+        message = f'Subject: Happy Birthday\n\n{contents}'
+        connection.sendmail(from_addr=mail, to_addrs=birthday_person['email'], msg=message)
