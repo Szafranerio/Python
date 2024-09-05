561b5849c5de4e2bbe63440624a7c91e6e6337f9
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-05 21:12:49 +0200

    control_updates

--- Python_Fun_Projects/Alien_Game/alien_invasion.py
+++ Python_Fun_Projects/Alien_Game/alien_invasion.py
@@ -16,13 +16,30 @@ class AlienInvastion:
 
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
+            self.ship.update()
+            
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+                    
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame. K_LEFT:
+                    self.ship.moving_left = False
 
-            self.screen.fill(self.bg_color)
-            self.ship.blitme()
-            pygame.display.flip()
+    def _update_screen(self):
+        self.screen.fill(self.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
