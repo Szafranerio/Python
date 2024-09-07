4c9485d67c5e799d7e14500ee74cc27153d91b86
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-07 11:33:16 +0200

    update

--- 100Days_Python_Projects/Day037_Piksela.py
+++ 100Days_Python_Projects/Day037_Piksela.py
@@ -2,8 +2,8 @@ import requests
 
 
 pixela_endpoint = 'https://pixe.la/v1/users'
-USER = 'szafranerio9'
-TOKEN = 'dasgfwagresvscsv'
+USER = 'szafraneriooo'
+TOKEN = 'vfbrgnthmyjntbrvetrbevs'
 
 
 user_params = {
@@ -14,5 +14,30 @@ user_params = {
 }
 
 
-response = requests.post(url=pixela_endpoint, json=user_params)
-print("User creation response:", response.status_code, response.text)
+#response = requests.post(url=pixela_endpoint, json=user_params)
+#print("User creation response:", response.status_code, response.text)
+
+
+graph_endpoint = f"{pixela_endpoint}/{USER}/graphs"
+
+graphs_params = {
+    'id' : 'graph2',
+    'name' : 'Pages of books',
+    'unit' : 'pages',
+    'type' : 'int',
+    'color' : 'momiji' ,
+}
+
+headers = {
+    "X-USER-TOKEN" : TOKEN
+}
+
+graph_response = requests.post(url=graph_endpoint, json=graphs_params, headers = headers)
+print(graph_response.text)
+
+point_params = {
+    'date' : '20240901',
+    'quantity' : 2,
+}
+point_response = requests.post(url='https://pixe.la/v1/users/szafraneriooo/graphs/graph2)', json=point_params, headers=headers)
+print(point_params)   
