bfb7337a22ebc4ecfcba00b4cb0bf6c0f76c6b75
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-09-17 21:35:04 +0200

    WebScraping

---
+++ 100Days_Python_Projects/WebScraping.py
@@ -0,0 +1,13 @@
+#sheet= https://forms.gle/djUzDvoccwE9X8S58
+
+from bs4 import BeautifulSoup
+import requests
+
+header =
+
+web = requests.get(url='https://www.zillow.com/san-francisco-ca/rentals/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22isMapVisible%22%3Atrue%2C%22mapBounds%22%3A%7B%22west%22%3A-122.49244517504883%2C%22east%22%3A-122.37399882495117%2C%22south%22%3A37.73622391764381%2C%22north%22%3A37.82915844492535%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A20330%2C%22regionType%22%3A6%7D%5D%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A13%7D')
+web = web.text
+
+web = BeautifulSoup(web, 'html.parser')
+link = web.select(".StyledPropertyCardWraper a")
+print(link)
